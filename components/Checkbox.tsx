0da52c26d7c06b6ca1ab364b236712a80792c75b
ibraSanusi <isanu227@gmail.com>
2025-12-26 17:51:01 +0100

    add name

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -4,13 +4,14 @@ interface Props {
   id: string;
   label: string;
   value: string;
+  name: string;
 }
 
-function Checkbox({ id, label, value }: Props) {
+function Checkbox({ id, label, value, name }: Props) {
   return (
     <div>
       <label htmlFor={id}>{label}</label>
-      <Input type="checkbox" id={id} value={value} />
+      <Input type="checkbox" id={id} name={name} value={value} />
     </div>
   );
 }
