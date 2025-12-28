8d549aec6c4b5399c5093432220fb4be008e11b5
ibraSanusi <isanu227@gmail.com>
2025-12-28 22:53:39 +0100

    update booking status when patience been contacted

--- lib/dashboard/whatsapp.ts
+++ lib/dashboard/whatsapp.ts
@@ -1,4 +1,19 @@
-export function contactViaWhatsapp(phone: string, message: string) {
+import confetti from "canvas-confetti";
+
+export async function contactViaWhatsapp(
+  phone: string,
+  message: string,
+  bookingId: number,
+  statusText: string
+) {
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-  window.open(url, "_blank");
+  //   window.open(url, "_blank");
+  const response = await fetch("/api/booking", {
+    method: "PUT",
+    body: JSON.stringify({ bookingId, statusText }),
+  });
+
+  if (!response.ok) return null;
+
+  confetti();
 }
