ecb94d834ab2d426bb484bf6acc9a3429c427303
ibraSanusi <isanu227@gmail.com>
2025-12-29 18:41:34 +0100

    refactor style

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,16 @@ function Header() {
   return (
     <header className="w-screen">
       <div className="max-w-360 w-full mx-auto flex flex-row justify-between p-8">
-        <span>imagen</span>
+        <span className="text-3xl">🦷</span>
         <div>
-          <nav className="flex flex-row gap-8 items-center">
-            <button>Uno</button>
-            <button>Dos</button>
-            <button>Tres</button>
+          <nav className="flex flex-row gap-8 items-center *:font-bold *:text-black/60">
+            <button>Ortodoncia</button>
+            <button>Radiología Digital</button>
+            <button>Anestesia General</button>
+            <button>Medicina Estética</button>
 
             <button className="p-4 rounded-lg bg-blue-600 text-white font-bold">
-              Explicame tu caso
+              Pedir cita
             </button>
           </nav>
         </div>
